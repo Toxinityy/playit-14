bf8fa225493e0a4ca7b7317a3cb4a728f4b07207
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-11-02 14:13:04 +0700

    add dropdown for alamat

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,11 @@
-'use client';
-import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { Button } from '@/components/ui/button';
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Alert, AlertDescription } from '@/components/ui/alert';
+"use client";
+import React, { useState, ChangeEvent, FormEvent, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Alert, AlertDescription } from "@/components/ui/alert";
+import axios from "axios";
 
 interface FormData {
   email: string;
@@ -20,13 +21,120 @@ interface Errors {
   username?: string;
 }
 
+interface Provinsi {
+  provinsi_id: number;
+  nama: string;
+}
+
+interface Kabupaten {
+  kabupaten_id: number;
+  nama: string;
+}
+
+interface Kecamatan {
+  kecamatan_id: number;
+  nama: string;
+}
+
+interface Kelurahan {
+  kelurahan_id: number;
+  nama: string;
+}
+
+const fetchProvinsi = async () => {
+  const { data: provinsiData } = await axios.get<Provinsi[]>("http://192.168.77.90:5000/api/provinsi");
+  return provinsiData;
+};
+
+const fetchKabupaten = async (provinsiId: number) => {
+  const { data: kabupatenData } = await axios.get<Kabupaten[]>(`http://192.168.77.90:5000/api/kabupaten/${provinsiId}`);
+  return kabupatenData;
+};
+
+const fetchKecamatan = async (kabupatenId: number) => {
+  const { data: kecamatanData } = await axios.get<Kecamatan[]>(`http://192.168.77.90:5000/api/kecamatan/${kabupatenId}`);
+  return kecamatanData;
+};
+
+const fetchKelurahan = async (kecamatanId: number) => {
+  const { data: kelurahan } = await axios.get<Kelurahan[]>(`http://192.168.77.90:5000/api/kelurahan/${kecamatanId}`);
+  return kelurahan;
+};
+
 const RegistrationPage = () => {
   const [formData, setFormData] = useState<FormData>({
-    email: '',
-    password: '',
-    confirmPassword: '',
-    username: '',
+    email: "",
+    password: "",
+    confirmPassword: "",
+    username: "",
   });
+
+  const [selectedKelurahanId, setSelectedKelurahanId] = useState<number>();
+  const [provinsi, setProvinsi] = useState<Provinsi[]>();
+  const [kabupaten, setKabupaten] = useState<Kabupaten[]>();
+  const [kecamatan, setKecamatan] = useState<Kecamatan[]>();
+  const [kelurahan, setKelurahan] = useState<Kelurahan[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const provinsiData = await fetchProvinsi();
+      setProvinsi(provinsiData);
+
+      const kabupatenData = await fetchKabupaten(provinsiData[0].provinsi_id);
+      setKabupaten(kabupatenData);
+
+      const kecamatanData = await fetchKecamatan(kabupatenData[0].kabupaten_id);
+      setKecamatan(kecamatanData);
+
+      const kelurahanData = await fetchKelurahan(kecamatanData[0].kecamatan_id);
+      setKelurahan(kelurahanData);
+
+      setSelectedKelurahanId(kelurahanData[0].kelurahan_id);
+    };
+    fetchData();
+  }, []);
+
+  const handleProvinsiChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedProvinsi = e.target.value;
+
+    const kabupatenData = await fetchKabupaten(Number(selectedProvinsi));
+    setKabupaten(kabupatenData);
+
+    const kecamatanData = await fetchKecamatan(kabupatenData[0].kabupaten_id);
+    setKecamatan(kecamatanData);
+
+    const kelurahanData = await fetchKelurahan(kecamatanData[0].kecamatan_id);
+    setKelurahan(kelurahanData);
+
+    setSelectedKelurahanId(kelurahanData[0].kelurahan_id);
+  };
+
+  const handleKabupatenChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedKabupaten = e.target.value;
+
+    const kecamatanData = await fetchKecamatan(Number(selectedKabupaten));
+    setKecamatan(kecamatanData);
+
+    const kelurahanData = await fetchKelurahan(kecamatanData[0].kecamatan_id);
+    setKelurahan(kelurahanData);
+    setSelectedKelurahanId(kelurahanData[0].kelurahan_id);
+  };
+
+  const handleKecamatanChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedKecamatan = e.target.value;
+
+    const kelurahanData = await fetchKelurahan(Number(selectedKecamatan));
+    setKelurahan(kelurahanData);
+    setSelectedKelurahanId(kelurahanData[0].kelurahan_id);
+  };
+
+  const handleKelurahanChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedKelurahan = e.target.value;
+
+    console.log(e.target.value);
+
+    setSelectedKelurahanId(Number(selectedKelurahan));
+  };
 
   const [errors, setErrors] = useState<Errors>({});
   const [success, setSuccess] = useState(false);
@@ -36,41 +144,52 @@ const RegistrationPage = () => {
 
     // Email validation
     if (!formData.email) {
-      newErrors.email = 'Email is required';
+      newErrors.email = "Email is required";
     } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      newErrors.email = 'Please enter a valid email';
+      newErrors.email = "Please enter a valid email";
     }
 
     // Username validation
     if (!formData.username) {
-      newErrors.username = 'Username is required';
+      newErrors.username = "Username is required";
     } else if (formData.username.length < 3) {
-      newErrors.username = 'Username must be at least 3 characters';
+      newErrors.username = "Username must be at least 3 characters";
     }
 
     // Password validation
     if (!formData.password) {
-      newErrors.password = 'Password is required';
+      newErrors.password = "Password is required";
     } else if (formData.password.length < 8) {
-      newErrors.password = 'Password must be at least 8 characters';
+      newErrors.password = "Password must be at least 8 characters";
     }
 
     // Confirm password validation
     if (formData.password !== formData.confirmPassword) {
-      newErrors.confirmPassword = 'Passwords do not match';
+      newErrors.confirmPassword = "Passwords do not match";
     }
 
     return newErrors;
   };
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     const newErrors = validateForm();
 
     if (Object.keys(newErrors).length === 0) {
+      // Here you would typically make an API call to register the user
+
+      const { data } = await axios.post("http://192.168.77.90:5000/api/register", {
+        nama: formData.username,
+        email: formData.email,
+        password: formData.password,
+        alamat_resmi: "jalan dodo no.5",
+        kelurahan_id: selectedKelurahanId,
+      });
+
+      console.log(formData);
+      console.log(selectedKelurahanId);
       setSuccess(true);
       setErrors({});
-      // Here you would typically make an API call to register the user
     } else {
       setErrors(newErrors);
       setSuccess(false);
@@ -93,7 +212,10 @@ const RegistrationPage = () => {
           <CardDescription>Enter your details to register</CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit} className="space-y-4">
+          <form
+            onSubmit={handleSubmit}
+            className="space-y-4"
+          >
             <div className="space-y-2">
               <Label htmlFor="username">Username</Label>
               <Input
@@ -102,11 +224,9 @@ const RegistrationPage = () => {
                 type="text"
                 value={formData.username}
                 onChange={handleChange}
-                className={errors.username ? 'border-red-500' : ''}
+                className={errors.username ? "border-red-500" : ""}
               />
-              {errors.username && (
-                <p className="text-sm text-red-500">{errors.username}</p>
-              )}
+              {errors.username && <p className="text-sm text-red-500">{errors.username}</p>}
             </div>
 
             <div className="space-y-2">
@@ -117,11 +237,97 @@ const RegistrationPage = () => {
                 type="email"
                 value={formData.email}
                 onChange={handleChange}
-                className={errors.email ? 'border-red-500' : ''}
+                className={errors.email ? "border-red-500" : ""}
               />
-              {errors.email && (
-                <p className="text-sm text-red-500">{errors.email}</p>
-              )}
+              {errors.email && <p className="text-sm text-red-500">{errors.email}</p>}
+            </div>
+
+            <div className="space-y-2">
+              <Label>Provinsi : </Label>
+              <select
+                name="provinsi"
+                onChange={(e) => {
+                  handleProvinsiChange(e);
+                }}
+                className="text-sm"
+              >
+                {provinsi?.map((p) => {
+                  return (
+                    <option
+                      value={p.provinsi_id}
+                      key={p.provinsi_id}
+                    >
+                      {p.nama}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+
+            <div className="space-y-2">
+              <Label>Kabupaten : </Label>
+              <select
+                name="kabupaten"
+                className="text-sm"
+                onChange={(e) => {
+                  handleKabupatenChange(e);
+                }}
+              >
+                {kabupaten?.map((p) => {
+                  return (
+                    <option
+                      value={p.kabupaten_id}
+                      key={p.kabupaten_id}
+                    >
+                      {p.nama}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+
+            <div className="space-y-2">
+              <Label>Kecamatan : </Label>
+              <select
+                name="kecamatan"
+                className="text-sm"
+                onChange={(e) => {
+                  handleKecamatanChange(e);
+                }}
+              >
+                {kecamatan?.map((p) => {
+                  return (
+                    <option
+                      value={p.kecamatan_id}
+                      key={p.kecamatan_id}
+                    >
+                      {p.nama}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+
+            <div className="space-y-2">
+              <Label>Kelurahan : </Label>
+              <select
+                name="kelurahan"
+                className="text-sm"
+                onChange={(e) => {
+                  handleKelurahanChange(e);
+                }}
+              >
+                {kelurahan?.map((p) => {
+                  return (
+                    <option
+                      value={p.kelurahan_id}
+                      key={p.kelurahan_id}
+                    >
+                      {p.nama}
+                    </option>
+                  );
+                })}
+              </select>
             </div>
 
             <div className="space-y-2">
@@ -132,11 +338,9 @@ const RegistrationPage = () => {
                 type="password"
                 value={formData.password}
                 onChange={handleChange}
-                className={errors.password ? 'border-red-500' : ''}
+                className={errors.password ? "border-red-500" : ""}
               />
-              {errors.password && (
-                <p className="text-sm text-red-500">{errors.password}</p>
-              )}
+              {errors.password && <p className="text-sm text-red-500">{errors.password}</p>}
             </div>
 
             <div className="space-y-2">
@@ -147,30 +351,32 @@ const RegistrationPage = () => {
                 type="password"
                 value={formData.confirmPassword}
                 onChange={handleChange}
-                className={errors.confirmPassword ? 'border-red-500' : ''}
+                className={errors.confirmPassword ? "border-red-500" : ""}
               />
-              {errors.confirmPassword && (
-                <p className="text-sm text-red-500">{errors.confirmPassword}</p>
-              )}
+              {errors.confirmPassword && <p className="text-sm text-red-500">{errors.confirmPassword}</p>}
             </div>
 
             {success && (
               <Alert className="bg-green-50">
-                <AlertDescription className="text-green-600">
-                  Registration successful! You can now login.
-                </AlertDescription>
+                <AlertDescription className="text-green-600">Registration successful! You can now login.</AlertDescription>
               </Alert>
             )}
 
-            <Button type="submit" className="w-full">
+            <Button
+              type="submit"
+              className="w-full"
+            >
               Register
             </Button>
           </form>
         </CardContent>
         <CardFooter className="flex justify-center">
           <p className="text-sm text-gray-600">
-            Already have an account?{' '}
-            <a href="/login" className="text-blue-600 hover:underline">
+            Already have an account?{" "}
+            <a
+              href="/login"
+              className="text-blue-600 hover:underline"
+            >
               Login here
             </a>
           </p>
